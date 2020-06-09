a82bc70878494cd022271897f05e4603d7043af8
Shaireen <jankowskam401@gmail.com>
2020-06-09 12:40:20 +0200

    small improvements - about page, schedule and home

--- js/script.js
+++ js/script.js
@@ -155,7 +155,7 @@ function handlePostData(PostDataHandled) {
                     document.querySelector(".debut-album .fp-header").textContent = theHeader;
 
                     //Since in this case wordpress gives us "marked up" text we need to use .innerHTML
-                    document.querySelector(".text-about-album").innerHTML = theExcerpt;
+                    document.querySelector(".text-about-album").innerHTML = theContent;
                 }
 
 
@@ -693,53 +693,6 @@ function showPressSlides(n) {
     pressDots[slidePressIndex - 1].className += " active";
 
 }
-
-
-
-
-
-
-
-
-
-//Vocal Quotes!
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-document.querySelector(".mySlides").style.display = "block";
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-
-
-
 
 
 
